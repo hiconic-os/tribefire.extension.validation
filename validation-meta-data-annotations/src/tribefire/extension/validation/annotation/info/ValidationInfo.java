9d6ed47efcb00bc27fdb7fcb414c34684fb24044
Christina Wilpernig <qriszdhina@gmail.com>
2026-03-20 14:54:34 +0100

    Added ValidationInfo meta data + annotation

---
+++ validation-meta-data-annotations/src/tribefire/extension/validation/annotation/info/ValidationInfo.java
@@ -0,0 +1,37 @@
+package tribefire.extension.validation.annotation.info;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * Attaches human-readable validation information.
+ *
+ * <p>
+ * When a property, or request element is annotated with @ValidationInfo, the provided text describes the expected validation criteria in a form
+ * intended for human understanding.
+ *
+ * <p>
+ * This annotation does not define or enforce validation rules. It serves purely as descriptive metadata and must be used in conjunction with actual
+ * validation mechanisms.
+ *
+ * <p>
+ * The information may be used by tooling to:
+ * <ul>
+ * <li>Enrich OpenAPI descriptions with validation guidance</li>
+ * <li>Display contextual hints or helper text in user interfaces</li>
+ * </ul>
+ *
+ * <p>
+ * The {@code globalId} attribute refers to the globalId of the related meta-data to maintain identity.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD, ElementType.FIELD, ElementType.TYPE })
+@Documented
+public @interface ValidationInfo {
+
+    String globalId() default "";
+}
---
+++ validation-meta-data-model/src/tribefire/extension/validation/model/meta/info/ValidationInfo.java
@@ -0,0 +1,34 @@
+package tribefire.extension.validation.model.meta.info;
+
+import com.braintribe.model.generic.i18n.LocalizedString;
+import com.braintribe.model.generic.reflection.EntityType;
+import com.braintribe.model.generic.reflection.EntityTypes;
+import com.braintribe.model.meta.data.UniversalMetaData;
+
+/**
+ * Human-readable information describing validation expectations.
+ * 
+ * <p>
+ * This metadata is intended for documentation and UI purposes only. It provides guidance to users, but does not represent or enforce the actual
+ * validation logic.
+ * 
+ * <p>
+ * The actual validation rules must be defined and enforced separately.
+ * 
+ * <p>
+ * Typical use cases:
+ * <ul>
+ *   <li>Displaying hints or helper text in user interfaces</li>
+ *   <li>Enriching API documentation with human-readable validation guidance</li>
+ * </ul>
+ */
+public interface ValidationInfo extends UniversalMetaData {
+
+	EntityType<ValidationInfo> T = EntityTypes.T(ValidationInfo.class);
+	
+	String text = "text";
+
+	void setText(LocalizedString text);
+	LocalizedString getText();
+	
+}
